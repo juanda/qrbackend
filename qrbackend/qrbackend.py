276e753127ffba08ec8c8f039994a2e3f073eeda
juanda <juanda@juandarodriguez.es>
2018-01-12 14:03:17 +0100

    ..

--- qrbackend/qrbackend.py
+++ qrbackend/qrbackend.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/generate-qr', methods=['POST'])
 def generate_qr():
     r = request.get_json()    
-    qr = get_qr_code(r['string'])
+    qr = get_qr_code(r['texto'])
     qr.save(os.path.dirname(os.path.abspath(__file__)) + '/static/qr.png') 
     url_qr = 'http://localhost:5000' +  url_for('static', filename='qr.png')
     resp = {"url": url_qr }
